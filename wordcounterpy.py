c4b7d1487e909531dfebe0a0d13115a38a6c0303
F. Javier Morales M <fmoralesm87@gmail.com>
2023-03-22 19:28:51 -0600

    Cambios minimos

--- wordcounterpy.py
+++ wordcounterpy.py
@@ -12,14 +12,23 @@ where()
 chdir('../')
 where()
 
-with open('main.tex', 'r', encoding='utf8') as file:
-    first = file.readline()
-    print(first)
 
-def get_numberline(phrase, file):
+def get_lines(phrase, file):
+    var = list()
     with open(file, 'r', encoding='utf8') as f:
-        for i, line in enumerate(f):
-            if phrase in line:
-                return i+1
+        for word in f.readlines():
+            if phrase in word:
+                var.append(word)
 
-get_numberline('capitulos', 'main.tex')
+    return var
+
+
+
+lines = get_lines('capitulos', 'main.tex')
+
+lines
+
+
+paths = [ele.strip().replace('% ','').replace('\\include{','').replace('}','') for ele in lines]
+pathswext = [i+'.tex' for i in paths]
+pathswext
